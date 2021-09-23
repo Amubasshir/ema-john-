ac8fd9c7b7d0ff04159d935defa5c3a9cd152833
Amubasshir <themubasshir07@gmail.com>
2021-09-23 16:25:09 +0600

    cart price updated

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -8,12 +8,21 @@ const Cart = (props) =>
     {
         total = total + product.price;
     }
+    const shipping = 15;
+    const tax = (total + shipping) * 10;
+    const grandTotal = total + shipping + tax;
+    
     return (
         <div>
             <h3>Order Summary</h3>
             <h5>Item Ordered: {props.cart.length}</h5>
             <br />
-            <p>total: { total}</p>
+            <p> total: {total.toFixed(2)}</p>
+            <p> Shipping: {shipping}</p>
+            <p>tax: {tax.toFixed(2)}</p>
+            <p>Grand Total{ grandTotal.toFixed(2)}</p>
+           
+           
        
         </div>
     );
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,9 +1,14 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Product.css';
+
+
 const Product = (props) =>
 {
     // console.log(props);
-    const { name, img, seller , price, stock } = props.product;
+    const { name, img, seller, price, stock } = props.product;
+    const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className="product">
             <div>
@@ -17,7 +22,7 @@ const Product = (props) =>
                 <button
                     onClick = { ()=>props.handleAddToCart(props.product)}
                     className="btn-regular"
-                >add to cart</button>
+                > {cartIcon}add to cart</button>
            </div>
 
         </div>
